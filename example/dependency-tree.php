7adf3732ad5e898032960985442b63eb34c89cd2
Anders Lövgren <andlov@nowise.se>
2021-11-24 20:13:50 +0100

    Use object references when adding dependency children.

--- example/dependency-tree.php
+++ example/dependency-tree.php
@@ -7,14 +7,22 @@ require_once(__DIR__ . "/../vendor/autoload.php");
 use UUP\BuildSystem\Tests\Depend;
 
 $t1 = new Depend("T1");
-$t2 = $t1->addChild(new Depend("T2"));
-$t3 = $t1->addChild(new Depend("T3"));
-$t4 = $t2->addChild(new Depend("T4"));
-$t6 = $t4->addChild(new Depend("T6"));
-$t5 = $t2->addChild(new Depend("T5"));
-$t5 = $t3->addChild(new Depend("T5"));
-$t7 = $t5->addChild(new Depend("T7"));
-$t8 = $t5->addChild(new Depend("T8"));
+$t2 = new Depend("T2");
+$t3 = new Depend("T3");
+$t4 = new Depend("T4");
+$t5 = new Depend("T5");
+$t6 = new Depend("T6");
+$t7 = new Depend("T7");
+$t8 = new Depend("T8");
+
+$t1->addChild($t2);
+$t1->addChild($t3);
+$t2->addChild($t4);
+$t4->addChild($t6);
+$t2->addChild($t5);
+$t3->addChild($t5);
+$t5->addChild($t7);
+$t5->addChild($t8);
 
 printf("++ Rebuild node T5:\n");
 $t5->getEvaluator()->rebuild();
