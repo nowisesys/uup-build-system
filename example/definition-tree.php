85168f10c6f3a4145bdd011daae8e20dcc85381f
Anders Lövgren <andlov@nowise.se>
2021-11-24 14:41:16 +0100

    Adding example directory.

---
+++ example/definition-tree.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+require_once(__DIR__ . "/../vendor/autoload.php");
+
+use UUP\BuildSystem\Node\DependencyTree;
+use UUP\BuildSystem\Tests\Definition;
+
+$tree = new DependencyTree();
+$tree->addDefinition(new Definition("T1"));
+$tree->addDefinition(new Definition("T2", ["T1"]));
+$tree->addDefinition(new Definition("T3", ["T1"]));
+$tree->addDefinition(new Definition("T4", ["T2"]));
+$tree->addDefinition(new Definition("T5", ["T2", "T3"]));
+$tree->addDefinition(new Definition("T6", ["T4"]));
+$tree->addDefinition(new Definition("T7", ["T5"]));
+$tree->addDefinition(new Definition("T8", ["T5"]));
+
+printf("++ Rebuild node T5:\n");
+$node = $tree->getRegistry()->getNode("T5");
+$node->getEvaluator()->rebuild();
+
+printf("++ Rebuild complete tree:\n");
+$tree->getEvaluator()->rebuild();
