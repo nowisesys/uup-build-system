9138b6f773dbfe1c9595d7bd9e0eeca21e69b326
Anders Lövgren <andlov@nowise.se>
2021-12-23 18:31:37 +0100

    Test classes for implicit make target rules.

---
+++ source/BuildSystem/Tests/Implicit/T6.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace UUP\BuildSystem\Tests\Implicit;
+
+use UUP\BuildSystem\Tests\Target;
+
+class T6 extends Target
+{
+    public function __construct()
+    {
+        parent::__construct("T6");
+    }
+}
