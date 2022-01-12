a8bfb66c09b7498162d3302173126501f4420242
Anders Lövgren <andlov@nowise.se>
2022-01-12 03:39:12 +0100

    Unit test for call target.

---
+++ tests/BuildSystem/Target/TargetCallTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace UUP\Tests\BuildSystem\Target;
+
+use PHPUnit\Framework\TestCase;
+use UUP\BuildSystem\Target\TargetCall;
+
+class TargetCallTest extends TestCase
+{
+    public function testGetName()
+    {
+        $target = new TargetCall();
+        $this->assertEquals("call", $target->getName());
+    }
+
+    public function testGetDescription()
+    {
+        $target = new TargetCall();
+        $this->assertEquals("Evaluate PHP code", $target->getDescription());
+    }
+
+    public function testIsUpdated()
+    {
+        $target = new TargetCall();
+        $this->assertFalse($target->isUpdated());
+    }
+
+    public function testRebuild()
+    {
+        $target = new TargetCall("printf('hello world!');");
+        $target->rebuild();
+        $this->assertTrue(true);
+    }
+
+    protected function setUp(): void
+    {
+        ob_start();     // Start output buffering
+    }
+
+    protected function tearDown(): void
+    {
+        ob_end_clean(); // Discard buffered output
+    }
+}
