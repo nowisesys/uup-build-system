3545df7cf4a9b2f3ff08362deddbbe0328b98953
Anders Lövgren <andlov@nowise.se>
2022-01-12 03:03:17 +0100

    Created unit test for phony target class.

---
+++ tests/BuildSystem/Target/TargetPhonyTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace UUP\Tests\BuildSystem\Target;
+
+use PHPUnit\Framework\TestCase;
+use UUP\BuildSystem\Target\TargetPhony;
+
+class TargetPhonyTest extends TestCase
+{
+    public function testGetName()
+    {
+        $target = new TargetPhony("test");
+        $this->assertEquals("test", $target->getName());
+    }
+
+    public function testGetDescription()
+    {
+        $target = new TargetPhony("test");
+        $this->assertEquals("Phony target for test", $target->getDescription());
+    }
+
+    public function testIsUpdated()
+    {
+        $target = new TargetPhony("test");
+        $this->assertFalse($target->isUpdated());
+    }
+
+    public function testRebuild()
+    {
+        $target = new TargetPhony("test");
+        $target->rebuild();
+        $this->assertTrue(true);
+    }
+}
